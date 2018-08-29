914160d1486401ba4ae5d605294cf47c13a4e82b
fjx <fjxdot@outlook.com>
2018-08-30 00:07:07 +0800

    修改表单不合法输入变红

--- src/componets/FormComponets.js
+++ src/componets/FormComponets.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup,FormControl,Col,ControlLabel,Button,Form, Checkbox} from 'react-bootstrap';
+import { FormGroup,FormControl,Col,ControlLabel,Button,Form, Checkbox,Alert} from 'react-bootstrap';
 import {request} from "../App";
 
 class FormComponets extends React.Component {
@@ -21,9 +21,27 @@ class FormComponets extends React.Component {
                     valid: false,
                     error: ''
                 }
+            },
+            loginErrorFlag: 'none'
+        }
+    }
+
+    getValidationState(stateFormField) {
+        switch (stateFormField) {
+            case 'username': {
+                if (this.state.form.username.valid === false) {
+                    return 'error'
+                }
+                break
+            }
+            case 'password': {
+                if (this.state.form.username.valid === false) {
+                    return 'error'
+                }
+                break
             }
         }
-
+        return null;
     }
 
     handleChange(stateFormField,value) {
@@ -76,9 +94,13 @@ class FormComponets extends React.Component {
 
     handleSubmit(e){
         e.preventDefault()
+        const {form} = this.state
         const {form:{username,password}} = this.state
+
         if (!username.valid || !password.valid) {
-            alert("请确认登录信息")
+            this.setState({
+                loginErrorFlag: true
+            })
             return
         }
         this.requestForLogin('POST',"http://localhost:3000/login",{
@@ -89,27 +111,28 @@ class FormComponets extends React.Component {
 
     render() {
         const {form: {username,password}} = this.state
-
         return (
             <Form horizontal>
-                <FormGroup controlId="formHorizontalEmail">
+                <FormGroup controlId="formHorizontalEmail" validationState={this.getValidationState('username')}>
                     <Col smOffset={3} componentClass={ControlLabel} sm={2}>
                         username
                     </Col>
                     <Col sm={3}>
-                        <FormControl type="string" placeholder="username" onChange={(e) => this.handleChange('username',e.target.value)}/>
+                        <FormControl type="string" placeholder={'username' || this.state.form.username.error} onChange={(e) => this.handleChange('username',e.target.value)}/>
                     </Col>
                 </FormGroup>
-
-                <FormGroup controlId="formHorizontalPassword">
+                <FormGroup controlId="formHorizontalPassword" validationState={this.getValidationState('password')}>
                     <Col smOffset={3} componentClass={ControlLabel} sm={2}>
                         Password
                     </Col>
                     <Col sm={3}>
-                        <FormControl type="password" placeholder="Password" onChange={(e) => this.handleChange('password',e.target.value)}/>
+                        <FormControl type="password" placeholder={'username' || this.state.form.username.error} onChange={(e) => this.handleChange('password',e.target.value)}/>
                     </Col>
                 </FormGroup>
-
+                {/*<span>{(!this.state.form.username.valid) || (!this.state.form.password.valid) ? '请输入正确的用户名和密码': null}</span>*/}
+                <Col smOffset={5} sm={3}>
+                    <Alert smOffset={3} bsStyle="danger" style={{display: this.state.loginErrorFlag}}>   '请输入正确的用户名和密码' </Alert>
+                </Col>
                 <FormGroup>
                     <Col smOffset={5} sm={10}>
                         <Checkbox>Remember me</Checkbox>
